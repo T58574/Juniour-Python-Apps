5ded95206d337b0a7d7ccb526ae973250adb49a8
T58 <64015619+T58574@users.noreply.github.com>
2023-01-16 12:22:14 +0500

    Update

--- Download&Convert2mp3.py
+++ Download&Convert2mp3.py
@@ -13,15 +13,25 @@ def convert_mp4_to_mp3(mp4_file, mp3_file):
     video.close()
     file = Path(mp4_file)
     file.unlink()
+    inputlag = input('\nRestart? n\y -> \n')
+    if inputlag == 'n':
+        pass
+    elif inputlag == 'y':
+        download_youtube()
+    else:
+        pass
 
 
 def download_youtube():
     url = str(input('Url -> '))
     yt = YouTube(url)
     yt.streams.filter(file_extension='mp4').get_highest_resolution().download()
-    mp4_file = f'{yt.title}.mp4'
-    mp3_file = f'Converted - {yt.title}.mp3'
+    yttitle = yt.title
+    yttitle = yttitle.replace('@/*#!$%^?\[]-_)+=;`~.,<>\'"|', '')
+    mp4_file = f'{yttitle}.mp4'
+    mp3_file = f'Converted - {yttitle}.mp3'
     convert_mp4_to_mp3(mp4_file, mp3_file)
 
 
 download_youtube()
+
