fb03079f4a95c0f27c15374f270bb463d64abae4
T58 <64015619+T58574@users.noreply.github.com>
2023-02-11 13:02:01 +0500

    Update Download video from YouTube.py

--- Download video from YouTube.py
+++ Download video from YouTube.py
@@ -1,10 +1,17 @@
 from pytube import YouTube
 
 def download_youtube():
-    url = str(input('Url -> '))
+    url = str(input('Url ->\n\n'))
     yt = YouTube(url)
     yt.streams.filter(file_extension='mp4').get_highest_resolution().download()
-    yttitle = yt.title
-    yttitle = yttitle.replace('@/*#!$%^?\[]-_)+=;`~.,<>\'"|', '')
+    print('Success\n')
 
-download_youtube()
+while True:
+    download_youtube()
+    more = input('more? y/n ->\n\n')
+    if more == 'y':
+        download_youtube()
+    elif more == 'n':
+        pass
+    else: break
+
