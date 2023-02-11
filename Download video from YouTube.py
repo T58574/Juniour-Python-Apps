3d2ef3cbab2fb5341114ff50cb44fcc3810a0f24
T58 <64015619+T58574@users.noreply.github.com>
2023-02-11 12:47:59 +0500

    Update Download video from YouTube.py

--- Download video from YouTube.py
+++ Download video from YouTube.py
@@ -1,8 +1,10 @@
 from pytube import YouTube
 
-def download_youtube(url):
+def download_youtube():
+    url = str(input('Url -> '))
     yt = YouTube(url)
-    yt.streams.filter(file_extension='mp4').get_highest_resolution().download('.\\cache\\')
-    print(f'\nFinished downloading:  {yt.title}')
+    yt.streams.filter(file_extension='mp4').get_highest_resolution().download()
+    yttitle = yt.title
+    yttitle = yttitle.replace('@/*#!$%^?\[]-_)+=;`~.,<>\'"|', '')
 
-download_youtube('Your url')
+download_youtube()
